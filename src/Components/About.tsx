2e457658989e477a43aa04d8da0b7fb02a8147a1
vivek00012 <35500124+vivek00012@users.noreply.github.com>
2024-09-30 15:11:48 +0530

    updated homepage

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -39,7 +39,7 @@ const About = () => {
                 </div>
                 <div className="h-fit flex justify-center items-center rounded-full bs:mr-10 w-fit">
                     <NeonGradientCard className="w-[325px] h-[325px] lg-mx:w-64 lg-mx:h-64 xsm-mx:w-56 xsm-mx:h-56 items-center justify-center text-center">
-                        <img className="w-full  h-full rounded-full " src="/profile.png" alt="profile" />
+                        <img className="w-full  h-full rounded-full " src="profile.png" alt="profile" />
                     </NeonGradientCard>
                 </div>
             </div>
